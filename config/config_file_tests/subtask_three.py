9d42764fb1d3ad1e6ee17e409a2c3aac5f06c197
Priya Sharma <priyash@sas.upenn.edu>
2021-06-28 17:58:22 -0400

    subtask ii and iii

--- config/config_file_tests/subtask_three.py
+++ config/config_file_tests/subtask_three.py
@@ -10,8 +10,8 @@ import pandas as pd
 
 from test_config_consistency import print_unique_values
 
-dili = pd.read_csv('/Users/priyash/Documents/ICEES_Project/ebcr0_datasets/DILI/augmentin_ptlist_upto_3.10.21_v2_ICEES header_rev2_add_year.csv')
-#covid = pd.read_csv('/Users/priyash/Documents/ICEES_Project/ebcr0_datasets/covid/patient.csv')
+#dili = pd.read_csv('/Users/priyash/Documents/ICEES_Project/ebcr0_datasets/DILI/augmentin_ptlist_upto_3.10.21_v2_ICEES header_rev2_add_year.csv')
+covid = pd.read_csv('/Users/priyash/Documents/ICEES_Project/ebcr0_datasets/covid/patient.csv', low_memory=False)
 
 
-print_unique_values(dili)
+print_unique_values(covid)
--- config/config_file_tests/subtask_two.py
+++ config/config_file_tests/subtask_two.py
@@ -10,9 +10,9 @@ import pandas as pd
 from test_config_consistency import difference_check
 
 
-dili = pd.read_csv('/Users/priyash/Documents/ICEES_Project/ebcr0_datasets/DILI/augmentin_ptlist_upto_3.10.21_v2_ICEES header_rev2_add_year.csv')
-#covid = pd.read_csv('/Users/priyash/Documents/ICEES_Project/ebcr0_datasets/covid/patient.csv')
+#dili = pd.read_csv('/Users/priyash/Documents/ICEES_Project/ebcr0_datasets/DILI/augmentin_ptlist_upto_3.10.21_v2_ICEES header_rev2_add_year.csv')
+covid = pd.read_csv('/Users/priyash/Documents/ICEES_Project/ebcr0_datasets/covid/patient.csv',low_memory=False)
 
 yaml_file = '/Users/priyash/Documents/GitHub/icees-api-config/config/all_features.yaml'
 
-difference_check(dili, yaml_file)
+difference_check(covid, yaml_file)
