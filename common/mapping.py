45a9cdf2ea687ac00dddb6096203220870ededfa
Hao Xu <xuh@cs.unc.edu>
2021-07-07 11:46:04 -0400

    update dhall scripts

--- common/mapping.py
+++ common/mapping.py
@@ -5,16 +5,17 @@ import subprocess
 import tempfile
 from collections import defaultdict
 
-input_file_path, output_file_path = sys.argv[1:]
+input_file_path, output_file_path_mapping, output_file_path_features, output_file_path_identifiers = sys.argv[1:]
 
 f, path = tempfile.mkstemp()
 os.close(f)
 
-subprocess.run(["dhall-to-yaml", "--file", input_file_path, "--output", path])
+subprocess.run(["dhall-to-yaml", "--quoted", "--file", input_file_path, "--output", path])
 
 with open(path) as inpf:
     dataset = yaml.safe_load(inpf)
 
+print(dataset)
 os.remove(path)
 
 fhirmapping = defaultdict(lambda: defaultdict(lambda: defaultdict(list)))
@@ -26,6 +27,8 @@ nearestfeaturemapping = defaultdict(lambda: {
     "attributes_to_features_map": {}
 })
 visitmapping = {}
+features = defaultdict(dict)
+identifiers = defaultdict(dict)
 
 def add_mapping(mapping, feature_name, nearestpointmapping):
     if "maximum" in mapping: 
@@ -75,7 +78,23 @@ for table in dataset:
                     visitmapping[feature_name] = mapping["param"]["param"]
                     
                 
-with open(output_file_path, "w") as outf:
+for table in dataset:
+    table_name = table["name"]
+    for feature in table["variables"]:
+        feature_name = feature["name"]
+        feature_out = {
+            "type": feature["feature"]["feature_type"]["tag"].lower(),
+            "categories": feature["feature"]["categories"]
+        }
+        for key in ["minimum", "maximum", "enum"]:
+            if key in feature["feature"]["feature_type"]["param"]:
+                feature_out[key] = feature["feature"]["feature_type"]["param"][key]
+        features[table_name][feature_name] = feature_out
+        
+        identifiers[table_name][feature_name] = feature["identifiers"]
+
+        
+with open(output_file_path_mapping, "w") as outf:
     yaml.dump({
         "FHIR": {k:{k2:dict(v2) for k2,v2 in v.items()} for k,v in fhirmapping.items()},
         "GEOID": dict(geoidmapping),
@@ -84,6 +103,13 @@ with open(output_file_path, "w") as outf:
         "Visit": visitmapping
     }, outf)
 
+with open(output_file_path_features, "w") as outf_features:
+    yaml.dump(dict(features), outf_features)
+    
+with open(output_file_path_identifiers, "w") as outf_identifiers:
+    yaml.dump(dict(identifiers), outf_identifiers)
+
+    
 
 
 
