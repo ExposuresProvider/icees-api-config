c29ddcf2779274a5f4434933d3ec3e6402cf1f1d
Meghamala Sinha <meghamala.sinha@gmail.com>
2021-08-30 13:56:50 -0700

    changed SCITD to SNOMED in identifiers.yaml file

--- config/config_file_tests/fix_inconsistencies.py
+++ config/config_file_tests/fix_inconsistencies.py
@@ -60,11 +60,59 @@ def check_consistant(table):
     return document
 
 
+def check_inconsistant(table):
+    #print(table)
+    for feature_name, identifier_list in table.items():
+        #print(table[feature_name])
+        new_list = []
+        for idx, identifier in enumerate(identifier_list):
+            #print(identifier)
+            item = str(identifier)
+            vocab = item.rsplit(':', 1)[0]
+            if vocab in allowedVocabs:
+                exists = "allowed"
+            else:
+                checkID, labels = checkIdExits(identifier)
+                exists = checkID
+            if not exists:
+                print(identifier)
+                new_list.append(identifier)
+        table[feature_name] = new_list
+        #print(table[feature_name])
+    return document
 
-with open(r'../identifiers copy.yml') as file:
-    document = yaml.full_load(file)
+def format_identifiers(document):
+    for table, feature in document.items():
+        for feature_name, identifier_list in list(feature.items()):
+            if identifier_list == []:
+                print(feature[feature_name])
+                del feature[feature_name]
+    return(document)
+
+#--------extract consistant identifiers
+#with open(r'../identifiers copy.yml') as file:
+#    document = yaml.full_load(file)
 #print(document['patient'])
 #d= check_consistant(document['patient'])
-d= check_consistant(document['visit'])
-with open(r'consistent_identifiers3.yml', 'w') as file:
+#d= check_consistant(document['visit'])
+#with open(r'consistent_identifiers3.yml', 'w') as file:
+#    documents = yaml.dump(d, file)
+
+
+#--------format 'consistent_identifiers_with_labels.yml' file to remove empty features
+#with open(r'consistent_identifiers_with_labels.yml') as file:
+#    document = yaml.full_load(file)
+#d = format_identifiers(document)
+#print(d)
+#with open(r'formatted_consistent_identifiers_with_labels.yml', 'w') as file:
+#    documents = yaml.dump(d, file)
+
+
+#--------extract inconsistant identifiers
+with open(r'../identifiers.yml') as file:
+    document = yaml.full_load(file)
+#d = check_inconsistant(document['patient'])
+d= check_inconsistant(document['visit'])
+print(d)
+with open(r'inconsistent_identifiers.yml', 'w') as file:
     documents = yaml.dump(d, file)
